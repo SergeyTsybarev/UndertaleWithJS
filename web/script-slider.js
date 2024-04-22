d57a60a87e9c9c8848231a6556a24b24508a701d
SergeyTsybarev <124906752+SergeyTsybarev@users.noreply.github.com>
2024-04-22 22:00:36 +0300

    Add files via upload

--- web/script-slider.js
+++ web/script-slider.js
@@ -1,16 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
 const priceValue = document.getElementById('priceValue');
 const priceSlider = document.getElementById('priceSlider');
 const products = document.querySelectorAll('.product[data-price]');
-
-priceSlider.addEventListener('input', function() {
-    products.forEach(product => {
-        const productPrice = parseInt(product.getAttribute('data-price'));
-        priceValue.textContent = priceSlider.value;
-
-        if (productPrice >= parseInt(priceSlider.value)) {
-            product.style.display = 'block';
-        } else {
-            product.style.display = 'none'; 
-        }
+let ShowFavorite = false;
+let ShowFigura = false;
+let Showother = false;
+document.getElementById('show-favorites-btn').addEventListener('click', function() {
+        ShowFavorite = !ShowFavorite;
+        updateProducts();
     });
+
+    document.getElementById('show-figura').addEventListener('click', function() {
+        ShowFigura = !ShowFigura;
+        updateProducts();
+    });
+
+    document.getElementById('show-other').addEventListener('click', function() {
+        Showother = !Showother;
+        updateProducts();
+    });
+
+    priceSlider.addEventListener('input', function() {
+        updateProducts();
+    });
+    
+    function updateProducts() {
+            products.forEach(product => {
+                const productPrice = parseInt(product.getAttribute('data-price'));
+                priceValue.textContent = priceSlider.value;
+
+                if (productPrice >= parseInt(priceSlider.value)) {
+                    let isFigura = JSON.parse(product.getAttribute('data-figura'));
+                    let isOther = JSON.parse(product.getAttribute('data-other'));
+                    let isFavorite = JSON.parse(product.getAttribute('data-favorite'));
+
+                    if ((ShowFigura && !isFigura) || (Showother && !isOther) || (ShowFavorite && !isFavorite)) {
+                        product.style.display = 'none';
+                    } else {
+                        product.style.display = 'block';
+                    }
+                } else {
+                    product.style.display = 'none';
+                }
+            });
+        }
 });
